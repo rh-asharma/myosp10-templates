65e3348bcdd9a451b4453e94746072a658ce0145
Amit <amit@redhat.com>
2017-03-20 12:56:16 -0400

    aa

---
+++ templates/openstack-tripleo-heat-templates/extraconfig/tasks/major_upgrade_block_storage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This runs an upgrade of Cinder Block Storage nodes.
+#
+set -eu
+
+# Special-case OVS for https://bugs.launchpad.net/tripleo/+bug/1635205
+special_case_ovs_upgrade_if_needed
+
+yum -y install python-zaqarclient  # needed for os-collect-config
+yum -y -q update
