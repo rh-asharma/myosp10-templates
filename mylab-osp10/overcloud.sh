642f12b3d293ff48072e8556161e91a64db8309a
Amit Sharma <rh-asharma@users.noreply.github.com>
2017-05-01 16:33:37 +0530

    Update overcloud.sh

--- mylab-osp10/overcloud.sh
+++ mylab-osp10/overcloud.sh
@@ -1,3 +1,3 @@
 
 
-openstack overcloud deploy --templates -r /home/stack/myosp10-templates/mylab-osp1/amit-custom-roles.yaml -e /home/stack/myosp10-templates/mylab-osp1/amit-lab-config.yaml  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml --control-scale 1  --compute-scale 1 --control-flavor control --compute-flavor compute --libvirt-type qemu  --neutron-network-type vxlan --neutron-tunnel-types vxlan
+openstack overcloud deploy --templates -r /home/stack/myosp10-templates/mylab-osp1/amit-custom-roles.yaml -e /home/stack/myosp10-templates/mylab-osp10/amit-lab-config.yaml  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml --control-scale 1  --compute-scale 1 --control-flavor control --compute-flavor compute --libvirt-type qemu  --neutron-network-type vxlan --neutron-tunnel-types vxlan
