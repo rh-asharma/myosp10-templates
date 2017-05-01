2089e319568663d965b11bb3b06635ccd99d6cc0
Amit Sharma <rh-asharma@users.noreply.github.com>
2017-05-01 16:23:10 +0530

    Create overcloud.sh

---
+++ mylab-osp10/overcloud.sh
@@ -0,0 +1,3 @@
+
+
+openstack overcloud deploy --templates -e /home/stack/myosp10-templates/ospd-8-9-10-multiple-nic-vlan/old-network-environment.yaml -e  /usr//share/openstack-tripleo-heat-templates/environments/storage-environment.yaml --control-scale 1  --compute-scale 1 --ceph-storage-scale 1 --control-flavor control --compute-flavor compute --ceph-storage-flavor ceph-storage --libvirt-type qemu  --neutron-network-type vxlan --neutron-tunnel-types vxlan
