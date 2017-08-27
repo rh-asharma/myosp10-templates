c832b413b01e94a24bbf846d17862fc594ae33f4
Amit Sharma <rh-asharma@users.noreply.github.com>
2017-08-27 15:30:48 +0530

    nsns

--- mylab-osp11/amit-deploy-multi-nic-aug24.sh
+++ mylab-osp11/amit-deploy-multi-nic-aug24.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+#!/bin/bash -x
 
-source ~/stackrc
 
-now=$(date +%F-%T)
 
-openstack overcloud deploy --templates -e /home/stack/myosp10-templates/mylab-osp11/amit-multi-nic-network-environment.yaml \
+if [ "$#" -lt 1 ]; then
+  echo "Pass: $0 deploy --> deploy RHOSP"
+  echo "Pass: $0 update --> update RHOSP"
+  exit 1
+fi
+
+test "$(whoami)" != 'stack' && (echo "This must be run by the stack user on the undercloud"; exit 1)
+
+function deployRHOSP {
+
+time openstack overcloud deploy --templates -e /home/stack/myosp10-templates/mylab-osp11/amit-multi-nic-network-environment.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/storage-environment.yaml \
 --stack amit-overcloud \
 --control-flavor control \
@@ -15,5 +23,18 @@ openstack overcloud deploy --templates -e /home/stack/myosp10-templates/mylab-os
 --compute-scale 1 \
 --ceph-storage-scale 1 \
 --libvirt-type qemu
+}
 
-# --dry-run \
+function updateRHOSP {
+time yes "" | openstack overcloud update stack overcloud -i --templates \
+  -e /home/stack/myosp10-templates/mylab-osp11/amit-multi-nic-network-environment.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/storage-environment.yaml
+}
+
+if [ $1 = "deploy" ]; then
+  deployRHOSP
+elif  [ $1 = "update" ]; then
+  updateRHOSP
+fi
+
+exit 0
